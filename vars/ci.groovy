5c5c2120e0e49cf842a9b8e160fc81a8a8ae01bc
Sampath Kumar <sampathkm09@gmail.com>
2023-03-10 02:00:27 +0800

    Commiting Changes to Ansible *.yml files

---
+++ vars/ci.groovy
@@ -0,0 +1,44 @@
+def call() {
+    pipeline {
+
+        agent {
+            label 'ansible'
+        }
+
+
+        stages {
+
+            stage('Compile/Build') {
+                steps {
+                    echo "compile"
+                }
+
+            }
+
+            stage('Unit Tests') {
+                steps {
+                    echo "Test"
+                }
+
+            }
+
+            stage('Quality Control') {
+                steps {
+                    echo "QA"
+                }
+            }
+
+            stage('Upload code to Centralized Place') {
+                steps {
+                    echo "Upload"
+                }
+
+            }
+
+
+        }
+
+
+    }
+
+}
