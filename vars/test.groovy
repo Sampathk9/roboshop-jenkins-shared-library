0b1d3876157ca94b66e26ebda3c7c0bc18b99d38
Sampath Kumar <sampathkm09@gmail.com>
2023-03-10 11:54:47 +0800

    Jenkins File Changes groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,8 +1,19 @@
-def abc =  "Hello"
-def xyz = 10
+def new(){
+
+    print "hello"
+    def xyz = "Hello"
+    print "xyz = {xyz}"
+    print "abc= {abc}"
 
 
-print "abc= ${abc}"
-print "xyz = ${xyz}"
+}
 
-print abc
+
+//def abc =  "Hello"
+//def xyz = 10
+//
+//
+//print "abc= ${abc}"
+//print "xyz = ${xyz}"
+//
+//print abc
