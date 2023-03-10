0bec82f4144df651c8c502c3919a7aa580f717fb
Sampath Kumar <sampathkm09@gmail.com>
2023-03-10 12:47:58 +0800

    Jenkins File Changes groovy

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,3 +1,5 @@
+/**To call other file we call it with file name "common" and the defined function "compile" in that common.compile */
+
 def call() {
     pipeline {
 
@@ -10,7 +12,10 @@ def call() {
 
             stage('Compile/Build') {
                 steps {
-                    echo "compile"
+                    script{
+                        common.compile
+
+                    }
                 }
 
             }
